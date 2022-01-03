2ae7c115f7250c10618bc2386ce62c376baf966e
Thomas Lafferty <tsl5046@gmail.com>
2022-01-03 02:23:40 -0500

    recipes built out and home page added

--- passrecipes/urls.py
+++ passrecipes/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from accounts.views import login_view
 from .views import home_view
 
 urlpatterns = [
     path('', home_view), 
     path('admin/', admin.site.urls),
+    path('login/', login_view),
     
 ]
