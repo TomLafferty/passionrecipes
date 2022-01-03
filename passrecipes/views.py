6fba9ac232cd16480e80a612caf75242eef434b1
Thomas Lafferty <tsl5046@gmail.com>
2022-01-03 00:56:36 -0500

    created super user and basic set up

--- passrecipes/views.py
+++ passrecipes/views.py
@@ -3,12 +3,13 @@ Render html web pages
 """
 
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 
+def home_view(request, *args, **kwargs):
 
-def home_view(request):
-
-    HTML_STRING = """
-    <h1>Cook Book Home</h1>
-    """
+    # HTML_STRING = """
+    # <h1>Cook Book Home</h1>
+    # """
+    HTML_STRING = render_to_string("home-view.html")
     return HttpResponse(HTML_STRING)
--- recipes/models.py
+++ recipes/models.py
@@ -4,26 +4,26 @@ from django.db import models
 
 
 
-class Recipe(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=220)
-    description = models.TextField(blank=True, null=True)
-    directions = models.TextField(blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True) 
-    updated = models.DateTimeField(auto_now=True) 
-    active = models.BooleanField(default=True)
+# class Recipe(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=220)
+#     description = models.TextField(blank=True, null=True)
+#     directions = models.TextField(blank=True, null=True)
+#     timestamp = models.DateTimeField(auto_now_add=True) 
+#     updated = models.DateTimeField(auto_now=True) 
+#     active = models.BooleanField(default=True)
 
 
-class RecipeIngredient(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    # recipe_id = models.AutoField -> ID to Recipe
-    name = models.CharField(max_length=220)
-    description = models.TextField(blank=True, null=True)
-    quantity = models.CharField(max_length=50, blank=True, null=True)  # 1 1/4
-    quantity_as_float = models.FloatField(blank=True, null=True)
-    # pounds, lbs, oz, gram, etc
-    unit = models.CharField(max_length=50, validators=[validate_unit_of_measure], blank=True, null=True)
-    directions = models.TextField(blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True) 
-    updated = models.DateTimeField(auto_now=True) 
-    active = models.BooleanField(default=True)
+# class RecipeIngredient(models.Model):
+#     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+#     # recipe_id = models.AutoField -> ID to Recipe
+#     name = models.CharField(max_length=220)
+#     description = models.TextField(blank=True, null=True)
+#     quantity = models.CharField(max_length=50, blank=True, null=True)  # 1 1/4
+#     quantity_as_float = models.FloatField(blank=True, null=True)
+#     # pounds, lbs, oz, gram, etc
+#     unit = models.CharField(max_length=50, validators=[validate_unit_of_measure], blank=True, null=True)
+#     directions = models.TextField(blank=True, null=True)
+#     timestamp = models.DateTimeField(auto_now_add=True) 
+#     updated = models.DateTimeField(auto_now=True) 
+#     active = models.BooleanField(default=True)
